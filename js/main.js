552a041e94e04edf37de74ce913f0d0d1d3fcb30
AviZeltser <avi.zeltser@gmail.com>
2014-12-24 19:42:53 +0600

    About

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Avi on 24.12.2014.
+ */
+$(document).ready(function() {
+    initSlider();
+})
+
+
+function initSlider() {
+    $('.js-slider').flexslider({
+        animation: "slide"
+    });
+}
